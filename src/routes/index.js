f37cc77979064268b21a12b8b2127b2db471f3cb
alejfj20 <auditsemseo@example.com>
2023-10-30 14:54:42 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userRouter = require('./user.router');
+
 
 // colocar las rutas aquí
+
+router.use('/user', userRouter);
 
 
 module.exports = router;
